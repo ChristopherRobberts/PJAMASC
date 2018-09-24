dbe0373a6210ddbdef2df3b5be5ccb9e94de727c
Sharif Sharif <sharifs@kth.se>
2018-09-24 16:34:26 +0200

    Updated database

---
+++ dump.sql
@@ -0,0 +1,115 @@
+-- MySQL dump 10.13  Distrib 5.7.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: new_schema
+-- ------------------------------------------------------
+-- Server version	5.7.23-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(244) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (2,'Dator'),(4,'Die Hard'),(5,'Die Hard 2'),(8,'Die Hard 3'),(6,'Frozen'),(7,'Frozen 2'),(3,'Hammock'),(9,'Hårtork'),(10,'Laptop'),(1,'TV');
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_list`
+--
+
+DROP TABLE IF EXISTS `product_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_list` (
+  `SKU_ID` varchar(50) NOT NULL,
+  `product` int(11) unsigned NOT NULL,
+  `owner` int(11) unsigned NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `image` varchar(30) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`SKU_ID`,`owner`),
+  KEY `FK_product_list_product` (`product`),
+  KEY `FK_product_list_user` (`owner`),
+  CONSTRAINT `FK_product_list_product` FOREIGN KEY (`product`) REFERENCES `product` (`id`),
+  CONSTRAINT `FK_product_list_user` FOREIGN KEY (`owner`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_list`
+--
+
+LOCK TABLES `product_list` WRITE;
+/*!40000 ALTER TABLE `product_list` DISABLE KEYS */;
+INSERT INTO `product_list` VALUES ('AB000001',2,7,'SUPER MEGA COMPUTER','/FILES',10,'2018-09-24 13:11:27','2018-09-24 13:11:33'),('AB000002',4,7,'AMAZING MOVIE','/files',5,'2018-09-24 13:15:03','2018-09-24 13:15:03'),('AB000003',5,7,'Sequel','/filespath',1,'2018-09-24 13:15:49','2018-09-24 13:15:50'),('AB00004',8,5,'Moviee','filepath',4,'2018-09-24 13:17:42','2018-09-24 13:17:42'),('AR123213',7,5,'Frozen 2 movie amazing sequel','filepath',19,'2018-09-24 13:19:34','2018-09-24 13:19:34'),('AT00001',6,5,'i forgot what this was','filepath',2,'2018-09-24 13:18:58','2018-09-24 13:18:58'),('GH123123',1,6,'SUPER AMOL LCD','filepath',32,'2018-09-24 13:23:26','2018-09-24 13:23:26'),('HJ123131',9,6,'oops','Filepath',12,'2018-09-24 13:25:08','2018-09-24 13:25:09'),('TY123455',3,6,'Hammock for relaxing','filepath',42,'2018-09-24 13:24:11','2018-09-24 13:24:11');
+/*!40000 ALTER TABLE `product_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `avatar` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (5,'JYSK','jysk@jysk.com','123456','/local'),(6,'Webhallen','contact@webhallen.com','6789','/local2'),(7,'Elgiganten','contact@elgiganten.se','12345','/local3/image2.jpeg');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-24 16:32:16
