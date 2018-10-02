f50a08adb604ba8424ea09eb3f155d51dfe621d6
Per Lovén <loven.per@gmail.com>
2018-10-02 15:15:17 +0200

    Add log out button and functionality. Very basic so far.

--- Cantaloupe/backend/routes/index.js
+++ Cantaloupe/backend/routes/index.js
@@ -5,55 +5,35 @@ let controller = require('../Controller/Controller.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    if(req.session.username){
-        res.redirect('/dashboard');
-    } else {
-        res.render('login');
-    }
+    res.render('login');
 });
 
-router.get('/dashboard', function(req, res) {
-    res.render('dashboard');
-});
-
+/* Trying to log in */
 router.post('/dashboard', function(req, res) {
     let username = req.body.uname;
     let password = req.body.pword;
-    req.session.username = username;
-    req.session.password = password;
 
     controller.login(username, password, function(loginSuccess){
         if(loginSuccess){
+            req.session.username = username;
+            req.session.password = password;
             console.log('going to dashboard');
-            res.render('dashboard', { 'username': username, 'password': password});
+            res.render('dashboard', { 'username': req.session.username, 'password': req.session.password});
         } else {
             console.log('going to login');
             res.render('login');
         };
-
     });
-    /* Validate login form input
-    req.checkBody('username', 'Username is required').notEmpty();
-    req.checkBody('password', 'Password is required').notEmpty();
-
-    let errors = req.validationErrors();
-
-    if(errors){
-
-        req.session.errors = errors;
-        req.session.success = false;
-        res.render('dashboard');
-
-    } else {
-
-        req.session.success = true;
-        res.render('/');
-    }
-    */
-
-    // Need to make a call to the controller to check if the user exists in the db
-    //res.render('dashboard', { 'username': username, 'password': password});
 });
 
+router.post('/logout', function(req, res){
+    req.session.username = null;
+    req.session.password = null;
+    res.render('login');
+})
+
+router.get('/logout', function(req, res){
+    res.render('login');
+})
 
 module.exports = router;
